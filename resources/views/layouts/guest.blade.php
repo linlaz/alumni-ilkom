dd56bc362cc2812489805bf154bcd7e1db021db9
linlaz <lazuardilintang@apps.ipb.ac.id>
2023-12-04 23:45:50 +0700

    title head && remove dark mode guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    @livewireStyles
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    @vite(['resources/scss/app.scss', 'resources/scss/themes/dark/app-dark.scss', 'resources/js/app.js', 'resources/js/script.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/script.js'])
     @livewireStyles
     @stack('style')
 
